be0763e3dc96c9633c00b0630915119b367232b1
Vitaliy Ukiru <33477790+vitaliy-ukiru@users.noreply.github.com>
2024-07-16 15:51:28 +0500

    feat(transport): add wrapper for api server

---
+++ internal/transport/webapi/middlewares/logger.go
@@ -0,0 +1,23 @@
+package middlewares
+
+import (
+	"github.com/labstack/echo/v4"
+	"github.com/vitaliy-ukiru/test-bank/pkg/logging"
+)
+
+func WrapRequestContextWithLogger(log logging.Logger) echo.MiddlewareFunc {
+	return func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			requestLogger := log
+			reqId := c.Response().Header().Get(echo.HeaderXRequestID)
+			if reqId != "" {
+				requestLogger = requestLogger.With(logging.String("request_id", reqId))
+			}
+			request := c.Request()
+			ctx := logging.Context(request.Context(), requestLogger)
+
+			c.SetRequest(request.WithContext(ctx))
+			return next(c)
+		}
+	}
+}
---
+++ internal/transport/webapi/router.go
@@ -0,0 +1,69 @@
+package webapi
+
+import (
+	"context"
+	"log/slog"
+	"net"
+	"strconv"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	slogecho "github.com/samber/slog-echo"
+	"github.com/vitaliy-ukiru/test-bank/internal/config"
+	"github.com/vitaliy-ukiru/test-bank/internal/transport/webapi/controllers"
+	"github.com/vitaliy-ukiru/test-bank/internal/transport/webapi/middlewares"
+	"github.com/vitaliy-ukiru/test-bank/pkg/logging"
+)
+
+type ApiRouter struct {
+	e                 *echo.Echo
+	cfg               config.Config
+	accountController *controllers.AccountController
+}
+
+func configureEcho(e *echo.Echo, cfg config.Config, logger logging.Logger) {
+	e.HideBanner = true
+	stdLog := logging.ConfigureLogLogger(logger, slog.LevelError)
+	e.Logger.SetLevel(99)
+	e.Server.ErrorLog = stdLog
+
+	level := slog.LevelError
+	if cfg.Env == config.EnvDev {
+		level = slog.LevelDebug
+	}
+
+	e.Use(slogecho.NewWithConfig(logger.ToStd(), slogecho.Config{
+		DefaultLevel:    level,
+		WithRequestBody: true,
+	}))
+
+	e.Use(middleware.Recover())
+	e.Use(middleware.RemoveTrailingSlash())
+	e.Use(middleware.RequestID())
+	e.Use(middlewares.WrapRequestContextWithLogger(logger))
+
+}
+
+func New(
+	cfg config.Config,
+	controller *controllers.AccountController,
+	logger logging.Logger,
+) *ApiRouter {
+	e := echo.New()
+	configureEcho(e, cfg, logger)
+	controller.Bind(e)
+
+	return &ApiRouter{
+		e:                 e,
+		cfg:               cfg,
+		accountController: controller,
+	}
+}
+
+func (a *ApiRouter) Start() error {
+	return a.e.Start(net.JoinHostPort(a.cfg.Server.Host, strconv.Itoa(a.cfg.Server.Port)))
+}
+
+func (a *ApiRouter) Shutdown(ctx context.Context) error {
+	return a.e.Shutdown(ctx)
+}
