569e6aca3c16c0c12a54b7a6fe20f0347c8ebdc5
Vitaliy Ukiru <33477790+vitaliy-ukiru@users.noreply.github.com>
2024-07-16 15:55:53 +0500

    feat: implement in-memory and postgres repository

---
+++ internal/infrastructure/repository/accounts/memory.go
@@ -0,0 +1,48 @@
+package accounts
+
+import (
+	"context"
+	"sync"
+
+	"github.com/vitaliy-ukiru/test-bank/internal/application"
+	"github.com/vitaliy-ukiru/test-bank/internal/domain/account"
+)
+
+type AccountStorage struct {
+	rw       sync.RWMutex
+	id       int64
+	accounts map[int64]float64
+}
+
+func NewInMemory() *AccountStorage {
+	return &AccountStorage{
+		accounts: make(map[int64]float64),
+	}
+}
+
+func (a *AccountStorage) GetAccountById(ctx context.Context, id int64) (account.Account, error) {
+	a.rw.RLock()
+	defer a.rw.RUnlock()
+	balance, ok := a.accounts[id]
+	if !ok {
+		return account.Account{}, application.ErrAccountNotFound
+	}
+
+	return account.NewAccount(id, balance), nil
+}
+
+func (a *AccountStorage) SaveAccount(ctx context.Context, acc account.Account) error {
+	a.rw.Lock()
+	a.rw.Unlock()
+	a.accounts[acc.Id()] = acc.GetBalance()
+	return nil
+}
+
+func (a *AccountStorage) NewAccount(ctx context.Context) (int64, error) {
+	a.rw.Lock()
+	a.rw.Unlock()
+	a.id++
+	id := a.id
+	a.accounts[id] = 0
+	return id, nil
+}
---
+++ internal/infrastructure/repository/accounts/postgres.go
@@ -0,0 +1,95 @@
+package accounts
+
+import (
+	"context"
+	"errors"
+	"fmt"
+
+	"github.com/jackc/pgtype/pgxtype"
+	"github.com/jackc/pgx/v4"
+	"github.com/vitaliy-ukiru/test-bank/internal/application"
+	"github.com/vitaliy-ukiru/test-bank/internal/domain/account"
+)
+
+type Connection interface {
+	pgxtype.Querier
+	BeginFunc(ctx context.Context, f func(pgx.Tx) error) (err error)
+}
+
+type Repository struct {
+	conn Connection
+}
+
+func NewRepository(conn Connection) *Repository {
+	return &Repository{conn: conn}
+}
+
+const opPrefix = "repo.Postgres."
+
+func (r *Repository) NewAccount(ctx context.Context) (int64, error) {
+	const op = opPrefix + "NewAccount"
+
+	row := r.conn.QueryRow(ctx, `INSERT INTO accounts(balance) VALUES(0) RETURNING id`)
+	var accountId int64
+	if err := row.Scan(&accountId); err != nil {
+		return 0, fmt.Errorf("%s:%w", op, err)
+	}
+	return accountId, nil
+}
+
+func (r *Repository) GetAccountById(ctx context.Context, id int64) (account.Account, error) {
+	const op = opPrefix + "GetAccountById"
+
+	row := r.conn.QueryRow(ctx, `SELECT id, balance FROM accounts WHERE id=$1`, id)
+
+	var (
+		accountId int64
+		balance   float64
+	)
+	if err := row.Scan(&accountId, balance); err != nil {
+		if errors.Is(err, pgx.ErrNoRows) {
+			return account.Account{}, fmt.Errorf("%s:%w", op, application.ErrAccountNotFound)
+		}
+
+		return account.Account{}, fmt.Errorf("%s:%w", op, err)
+	}
+
+	return account.NewAccount(id, balance), nil
+
+}
+func (r *Repository) SaveAccount(ctx context.Context, acc account.Account) error {
+	const op = opPrefix + "SaveAccount"
+
+	_, err := r.conn.Exec(ctx, `UPDATE accounts SET balance=$2 WHERE id=$1`, acc.Id(), acc.GetBalance())
+	if err != nil {
+		return fmt.Errorf("%s:%w", op, err)
+	}
+	return nil
+}
+
+func (r *Repository) Acquire(ctx context.Context, id int64, fn application.AccountProcessFunc) (err error) {
+	err = r.conn.BeginFunc(ctx, func(tx pgx.Tx) error {
+		wrapped := r.with(tx)
+		a, err := wrapped.GetAccountById(ctx, id)
+		if err != nil {
+			return err
+		}
+		if err := fn(&a); err != nil {
+			return err
+		}
+		if err := wrapped.SaveAccount(ctx, a); err != nil {
+			return err
+		}
+		return nil
+	})
+
+	if err != nil {
+		return err
+	}
+	return nil
+
+}
+
+func (r *Repository) with(tx pgx.Tx) *Repository {
+	return &Repository{conn: tx}
+}
